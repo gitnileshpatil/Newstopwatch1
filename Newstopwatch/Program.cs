eca8d7461b389e85bdeb3663fffb459ff3f389d3
Nilesh <patil_nilesh55@ymail.com>
2020-10-05 19:08:10 +0530

    Watch commit

---
+++ Newstopwatch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Newstopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch instance = new StopWatch();
+            instance.Start();
+            Thread.Sleep(3000);
+            instance.Stop();
+
+            Console.WriteLine("Interval:" + instance.GetInterval());
+            Console.ReadKey();
+
+        }
+    }
+
+    public class StopWatch
+    {
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        bool IsStarted { get; set; }
+
+        public void Start()
+        {
+            if(!IsStarted)
+            {
+                StartTime = DateTime.Now;
+                IsStarted = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if (IsStarted)
+            {
+                EndTime = DateTime.Now;
+                IsStarted = false;
+            }
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return EndTime - StartTime;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UjjwalaProgram
+{
+    class Stopwatch
+    {
+        DateTime StartTime;
+        DateTime StopTime;
+        Boolean IsRunning;
+
+        public void Start()
+        {
+            if (IsRunning)
+            {
+                throw new InvalidOperationException("Cannot start : Already Running");
+            }
+
+            StartTime = DateTime.Now;
+            IsRunning = false;
+
+        }
+
+        public void Stop()
+        {
+            if(!IsRunning)
+            {
+                throw new InvalidOperationException("Cannot Stop : Not Running");
+                
+            }
+
+            
+            StopTime = DateTime.Now;
+            IsRunning = true;
+        }
+
+        public TimeSpan ElapsedTime()
+        {
+            if(IsRunning)
+            {
+                return DateTime.Now - StartTime;
+            }
+
+            else
+            {
+                return StopTime - StartTime;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var Stopwatch = new Stopwatch();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("1-Start Timer \n 2- stop Timer \n 3-Read Timer \n 0- Quit \n");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1: Stopwatch.Start(); break;
+                    case 2: Stopwatch.Stop(); break;
+                    case 3: Console.WriteLine(Stopwatch.ElapsedTime()); break;
+                }
+
+            } while (choice != 0);
+
+            
+        }
+    }
+}
